f556f74bf14b2ea01aa1bded29f4cf16efb91f72
seb-larue96 <larue.sebastien@dsi-oi.com>
2026-02-09 14:49:24 +0400

    update user controller

--- src/application/users/users.controller.ts
+++ src/application/users/users.controller.ts
@@ -1,34 +1,58 @@
 import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { ApiOperation, ApiParam, ApiResponse, ApiTags } from '@nestjs/swagger';
 import { UserService } from './users.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
 
+@ApiTags('users')
 @Controller('users')
 export class UsersController {
   constructor(private readonly usersService: UserService) {}
 
-  @Post()
-  create(@Body() createUserDto: CreateUserDto) {
+  @Post('createUser')
+  @ApiOperation({ summary: 'Create a new user' })
+  @ApiResponse({ status: 201, description: 'The user has been successfully created.' })
+  @ApiResponse({ status: 400, description: 'Bad Request.' })
+  @ApiResponse({ status: 403, description: 'Forbidden.' })
+  async create(@Body() createUserDto: CreateUserDto) {
     return this.usersService.create(createUserDto);
   }
 
-  @Get()
-  findAll() {
+  @Get('getUsers')
+  @ApiOperation({ summary: 'Get all users' })
+  @ApiResponse({ status: 200, description: 'List of all users.' })
+  @ApiResponse({ status: 403, description: 'Forbidden.' })
+  async findAll() {
     return this.usersService.findAll();
   }
 
-  @Get(':id')
-  findOne(@Param('id') id: number) {
+  @Get('getUserById/:id')
+  @ApiOperation({ summary: 'Get user by id' })
+  @ApiParam({ name: 'id', type: Number, description: 'The id of the user to retrieve.' })
+  @ApiResponse({ status: 403, description: 'Forbidden.' })
+  @ApiResponse({ status: 200, description: 'The user has been successfully retrieved.' })
+  @ApiResponse({ status: 404, description: 'User not found.' })
+  async findOneById(@Param('id') id: number) {
     return this.usersService.findOneById(id);
   }
 
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
-    return this.usersService.update(+id, updateUserDto);
+  @Patch('updateUser/:id')
+  @ApiOperation({ summary: 'Update existing user by id' })
+  @ApiParam({ name: 'id', type: Number, description: 'The id of the user to update.' })
+  @ApiResponse({ status: 200, description: 'The user has been successfully updated.' })
+  @ApiResponse({ status: 403, description: 'Forbidden.' })
+  @ApiResponse({ status: 404, description: 'User not found.' })
+  async update(@Param('id') id: number, @Body() updateUserDto: UpdateUserDto) {
+    return this.usersService.update(id, updateUserDto);
   }
 
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.usersService.remove(+id);
+  @Delete('removeUser/:id')
+  @ApiOperation({ summary: 'Remove user by id' })
+  @ApiParam({ name: 'id', type: Number, description: 'The id of the user to remove.' })
+  @ApiResponse({ status: 200, description: 'The user has been successfully removed.' })
+  @ApiResponse({ status: 403, description: 'Forbidden.' })
+  @ApiResponse({ status: 404, description: 'User not found.' })
+  async remove(@Param('id') id: number) {
+    return this.usersService.remove(id);
   }
 }
